ab3fff8ae08fda9f7b37b028de9f5233c6d1fcc1
JMCAbato <jmcabato@hotmail.com>
2026-02-25 11:12:31 +0100

    chore: update cache version

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@
  * Provides offline functionality and asset caching
  */
 
-const CACHE_NAME = 'taxi-pwa-v42-money-inputs';
-const RUNTIME_CACHE = 'taxi-runtime-v20-money-inputs';
+const CACHE_NAME = 'taxi-pwa-v43-hide-ui-no-auth';
+const RUNTIME_CACHE = 'taxi-runtime-v21-hide-ui-no-auth';
 
 // Assets to cache on install
 const PRECACHE_ASSETS = [
