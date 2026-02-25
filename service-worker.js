08c9d4dcbf719d3ca3ca6b9b4b7a9ec89bf36f9c
JMCAbato <jmcabato@hotmail.com>
2026-02-25 15:13:52 +0100

    chore: update cache version

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@
  * Provides offline functionality and asset caching
  */
 
-const CACHE_NAME = 'taxi-pwa-v45-toast-position-fix';
-const RUNTIME_CACHE = 'taxi-runtime-v23-toast-position-fix';
+const CACHE_NAME = 'taxi-pwa-v46-reconciliation-detail';
+const RUNTIME_CACHE = 'taxi-runtime-v24-reconciliation-detail';
 
 // Assets to cache on install
 const PRECACHE_ASSETS = [
