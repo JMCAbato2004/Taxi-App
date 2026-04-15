86595199e5e382e1a7f5c8c09d007b4897d2f0ed
JMCAbato <jmcabato@hotmail.com>
2026-04-15 20:46:41 +0200

    chore: bump cache version to force SW update on gh-pages

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@
  * Provides offline functionality and asset caching
  */
 
-const CACHE_NAME = 'taxi-pwa-v53-datetime-filters';
-const RUNTIME_CACHE = 'taxi-runtime-v31-datetime-filters';
+const CACHE_NAME = 'taxi-pwa-v54-ajustes-20260415';
+const RUNTIME_CACHE = 'taxi-runtime-v32-ajustes-20260415';
 
 // Assets to cache on install
 const PRECACHE_ASSETS = [
